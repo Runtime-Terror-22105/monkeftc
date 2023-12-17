9c03a15334951b3390e92f3bc6eb35f2a4c6a81b
tarunja1ks <tarunjaiks@gmail.com>
2023-12-16 23:21:41 -0800

    moreSliderightteleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTeleop.java
@@ -82,12 +82,14 @@ public class DriveTeleop extends LinearOpMode  {
             telemetry.update();
             if(gamepad1.left_bumper){
                 robot.SlideLeft.setPower(-1.0);
+//                robot.SlideRight.setPower(-1.0);
                 telemetry.addData("SHOWS UPDATE","SLIDEPRESSED");
                 telemetry.update();
             }
 
             else if(gamepad1.right_bumper){
                 robot.SlideLeft.setPower(1.0);
+//                robot.SlideRight.setPower(1.0);
                 telemetry.addData("SHOWS UPDATE","SLIDEPRESSED");
                 telemetry.update();
             }

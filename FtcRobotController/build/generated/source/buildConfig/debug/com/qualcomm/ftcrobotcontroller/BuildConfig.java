17200980fb94b3493da4976c13827c7f2fa08517
tarunja1ks <tarunjaiks@gmail.com>
2023-12-16 17:37:49 -0800

    try

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2023-12-15T23:22:08.707-0800";
+  public static final String APP_BUILD_TIME = "2023-12-16T17:33:12.784-0800";
 }

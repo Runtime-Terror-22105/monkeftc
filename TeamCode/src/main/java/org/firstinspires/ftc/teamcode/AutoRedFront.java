040a3a33de5362ab6061c2db6b69d79ad6aafbf1
aaguy-hue <aaditmathur6@gmail.com>
2023-12-17 09:13:56 -0800

    fixed all the functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedFront.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedFront.java
@@ -187,17 +187,32 @@ public class AutoRedFront extends LinearOpMode {
     }
 
     public void turnLeft(double power) {
-        spin(power);
+        spinLeft(power);
         long sleeptime = (long)(RIGHTANGLETURNTIME/power); // tweak this value with trial and error
         sleep(sleeptime);
         CancelPowerRobot();
     }
 
     public void turnRight(double power) {
-        spin(power);
+        spinRight(power);
         long sleeptime = (long)(RIGHTANGLETURNTIME/power);
         sleep(sleeptime);
         CancelPowerRobot();
+    }
+
+    public void intake(int milliseconds) {
+        /**
+         * @param milliseconds - How long to intake for, in milliseconds
+         */
+        robot.intake.setPower(-1.0);
+        robot.intakeControl.setPosition(0.5);
+        robot.WheelLeft.setPower(1.0);
+        robot.WheelRight.setPower(-1.0);
+        sleep(milliseconds);
+        robot.intake.setPower(1.0);
+        robot.intakeControl.setPosition(0.5);
+        robot.WheelLeft.setPower(-1.0);
+        robot.WheelRight.setPower(1.0);
     }
 
     public void intake(int milliseconds, double power) {
@@ -205,34 +220,48 @@ public class AutoRedFront extends LinearOpMode {
          * @param milliseconds - How long to intake for, in milliseconds
          * @param power - The power to use for the motors
          */
-        robot.intake.setPower(power);
+        robot.intake.setPower(-power);
+        robot.intakeControl.setPosition(0.5);
+        robot.WheelLeft.setPower(power);
+        robot.WheelRight.setPower(-power);
         sleep(milliseconds);
-        robot.intake.setPower(0);
+        robot.intake.setPower(power);
+        robot.intakeControl.setPosition(0.5);
+        robot.WheelLeft.setPower(-power);
+        robot.WheelRight.setPower(power);
     }
 
-    public void outtake(int milliseconds, double power) {
-        /**
-         * @param milliseconds - How long to outtake for, in milliseconds
-         * @param power - The power to use for the servos
-         */
-        robot.intake.setPower(power);
-        sleep(milliseconds);
-        robot.intake.setPower(0);
-    }
+//    public void outtake(int milliseconds, double power) {
+//        /**
+//         * @param milliseconds - How long to outtake for, in milliseconds
+//         * @param power - The power to use for the servos
+//         */
+//        robot.intake.setPower(power);
+//        sleep(milliseconds);
+//        robot.intake.setPower(0);
+//    }
 
-    public void spin(double power) {
-        power=-power;
-        robot.motorFrontLeft.setPower(power);
+    public void spinLeft(double power) {
+//        power=-power;
+        robot.motorFrontLeft.setPower(-power);
         robot.motorFrontRight.setPower(-power);
-        robot.motorBackRight.setPower(power);
         robot.motorBackLeft.setPower(power);
+        robot.motorBackRight.setPower(power);
+    }
+
+    public void spinRight(double power) {
+//        power=-power;
+        robot.motorFrontLeft.setPower(power);
+        robot.motorFrontRight.setPower(power);
+        robot.motorBackLeft.setPower(-power);
+        robot.motorBackRight.setPower(-power);
     }
 
     public void FrontDrive(double power) {
-        robot.motorFrontLeft.setPower(power);
+        robot.motorFrontLeft.setPower(-power);
         robot.motorFrontRight.setPower(power);
-        robot.motorBackRight.setPower(power);
         robot.motorBackLeft.setPower(power);
+        robot.motorBackRight.setPower(-power);
     }
 
     public void LeftSlantDrive(double power) {
@@ -273,21 +302,20 @@ public class AutoRedFront extends LinearOpMode {
     }
 
     public void SlideRight(double power) {
-        power = -power;
-        robot.motorFrontLeft.setPower(power);
+//        power = -power;
+        robot.motorFrontLeft.setPower(-power);
         robot.motorFrontRight.setPower(-power);
-        robot.motorBackRight.setPower(-power);
         robot.motorBackLeft.setPower(-power);
+        robot.motorBackRight.setPower(-power);
 
     }
 
     public void SlideLeft(double power) {
-        power = -power;
-        robot.motorFrontLeft.setPower(-power);
+//        power = -power;
+        robot.motorFrontLeft.setPower(power);
         robot.motorFrontRight.setPower(power);
-        robot.motorBackRight.setPower(power);
         robot.motorBackLeft.setPower(power);
-
+        robot.motorBackRight.setPower(power);
     }
 
     public void CancelPowerRobot() {

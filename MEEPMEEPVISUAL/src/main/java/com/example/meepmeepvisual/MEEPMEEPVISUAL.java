0e29a92170c91f4a4defb10f753edf6e661abcd2
aaguy-hue <aaditmathur6@gmail.com>
2023-12-22 15:36:23 -0800

    add roadrunner

--- MEEPMEEPVISUAL/src/main/java/com/example/meepmeepvisual/MEEPMEEPVISUAL.java
+++ MEEPMEEPVISUAL/src/main/java/com/example/meepmeepvisual/MEEPMEEPVISUAL.java
@@ -1,6 +1,7 @@
 package com.example.meepmeepvisual;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -14,14 +15,7 @@ public class MEEPMEEPVISUAL {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                                .splineTo(new Vector2d(-36.19, -28.34), Math.toRadians(91.11))
                                 .build()
                 );
 
